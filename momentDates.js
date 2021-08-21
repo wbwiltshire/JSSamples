0a71a34b9aaea0fe0aa5d92a702ed5617fc3c179
wbwiltshire <wbwiltshire@hotmail.com>
2021-08-21 16:09:31 -0400

    Updated for DateTime example

--- momentDates.js
+++ momentDates.js
@@ -6,7 +6,6 @@ var today = new Date();
 console.log("\tNumber of ticks - Number(today): " + Number(today))
 var todayDateFormatted = ''
 var todayTimeFormatted = ''
-var todayFormatted = Number(today);
 
 todayDateFormatted = today.toLocaleDateString('en');
 console.log("\tToday formatted as Locale date: " + todayDateFormatted)
@@ -18,7 +17,6 @@ console.log();
 console.log('Javascript UTC dates');
 var todayUTC = new Date(Date.UTC(2019, 12, 28, 0, 0, 0));
 console.log("\tToday(UTC) date: " + todayUTC.toUTCString('en-EN'));
-
 
 console.log();
 console.log('Moment dates');
@@ -33,3 +31,14 @@ console.log("\tFormatted String date('2019-12-28 00:00:00.000'): " + mTodayStrin
 var mTodayUTCString = moment.utc(dateString);
 console.log("\tUTC string date('2019-12-28 00:00:00.000'): " + mTodayUTCString.toString());
 console.log("\tFormatted UTC String date('2019-12-28 00:00:00.000'): " + mTodayUTCString.format('MM/DD/YYYY HH:mm:ss'));
+
+console.log('Moment datetime');
+// Number of milliseconds since 1/1/1970
+var todayAsNumber = Number(today);
+var mDateTimeString = moment().format('MM/DD/YYYY HH:mm')
+console.log("\tToday: " + mDateTimeString.toString());
+mDateTimeString = moment(todayAsNumber).format('MM/DD/YYYY HH:mm')
+console.log("\tToday: " + mDateTimeString.toString());
+dateTimeAsNumber = 1629588600 * 1000;
+mDateTimeString = moment(dateTimeAsNumber).format('MM/DD/YYYY HH:mm')
+console.log("\tDateTime: " + mDateTimeString.toString());
